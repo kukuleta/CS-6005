47b50953bba9699a7b25052e81391db507b6aab0
kukuleta <ismaildennizli@gmail.com>
2019-10-26 19:10:30 +0300

    Write test cases for Extract and Filter class

---
+++ ps1/test/twitter/ExtractTest.java
@@ -0,0 +1,83 @@
+/* Copyright (c) 2007-2016 MIT 6.005 course staff, all rights reserved.
+ * Redistribution of original or derived work requires permission of course staff.
+ */
+package twitter;
+
+import static org.junit.Assert.*;
+
+import java.time.Instant;
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.Collections;
+import java.util.List;
+import java.util.Set;
+
+import org.junit.Test;
+
+public class ExtractTest {
+
+    /*
+     * Partition the inputs as follows;
+     * 
+     * TODO: your testing strategies for these methods should go here.
+     * See the ic03-testing exercise for examples of what a testing strategy comment looks like.
+     * Make sure you have partitions.
+     */
+    
+    private static final Instant d1 = Instant.parse("2016-02-17T10:00:00Z");
+    private static final Instant d2 = Instant.parse("2016-02-17T11:00:00Z");
+    private static final Instant d3 = Instant.parse("2016-02-17T12:00:00Z");
+    
+    private static final Tweet tweet1 = new Tweet(1, "alyssa", "is it reasonable to talk about rivest so much?", d1);
+    private static final Tweet tweet2 = new Tweet(2, "bbitdiddle", "rivest talk in 30 minutes #hype", d2);
+    private static final Tweet tweet3 = new Tweet(3, "kukkuleta","What the heck is those tests",d3);
+    
+    @Test(expected=AssertionError.class)
+    public void testAssertionsEnabled() {
+        assert false; // make sure assertions are enabled with VM argument: -ea
+    }
+    
+    @Test
+    public void testGetTimespanTwoTweets() {
+        Timespan timespan = Extract.getTimespan(Arrays.asList(tweet1, tweet2));
+        
+        assertEquals("expected start", d1, timespan.getStart());
+        assertEquals("expected end", d2, timespan.getEnd());
+    }
+    
+    @Test
+    public void testGetTimespanCoveringMultipleTweets()
+    {
+    	Timespan timespan = Extract.getTimespan(Arrays.asList(tweet1,tweet2,tweet3));
+    	List<Instant> Instants = new ArrayList<Instant>();
+    	Instants.add(d1);
+    	Instants.add(d2);
+    	Instants.add(d3);
+    	Collections.sort(Instants);
+    	System.out.println(Instants);
+    	assertEquals("Expected start",d1, timespan.getStart());
+    	assertEquals("Expected end",d3,timespan.getEnd());
+    }
+    
+    @Test
+    public void testGetMentionedUsersNoMention() {
+        Set<String> mentionedUsers = Extract.getMentionedUsers(Arrays.asList(tweet1));
+        
+        assertTrue("expected empty set", mentionedUsers.isEmpty());
+    }
+
+    /*
+     * Warning: all the tests you write here must be runnable against any
+     * Extract class that follows the spec. It will be run against several staff
+     * implementations of Extract, which will be done by overwriting
+     * (temporarily) your version of Extract with the staff's version.
+     * DO NOT strengthen the spec of Extract or its methods.
+     * 
+     * In particular, your test cases must not call helper methods of your own
+     * that you have put in Extract, because that means you're testing a
+     * stronger spec than Extract says. If you need such helper methods, define
+     * them in a different class. If you only need them in this test class, then
+     * keep them in this test class.
+     */
+
+}
